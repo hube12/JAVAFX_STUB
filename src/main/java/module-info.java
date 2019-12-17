c771dae3eb927fbbbec36f24ed1ef08ccd200de3
KESSLER Erwan <erwan.kessler@telecomnancy.eu>
2019-12-17 08:11:29 +0100

    Running version 11 for more compatibility

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-@SuppressWarnings("module")
-module Podcast_Collector {
-    requires javafx.controls;
-    requires javafx.fxml;
-    
-    opens edu.PCD.PodcastCollector to javafx.fxml;
-    exports edu.PCD.PodcastCollector;
-}
